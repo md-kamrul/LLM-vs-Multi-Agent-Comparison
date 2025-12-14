c005f57e140e24796487a1f511965db0dbad0694
md-kamrul <join.kamrul@gmail.com>
2025-12-14 23:46:53 +0600

    add: root agent design done

---
+++ MultiAgent/agent.py
@@ -0,0 +1,21 @@
+from google.adk.agents.llm_agent import Agent
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='summarization_agent_main',
+    description='An agent that summarizes texts or paragraphs using multi-agents.',
+    instruction="""
+    You are a highly skilled summarization agent. Your task is to operate and manage all the agents related to the summarization process. 
+    
+    - You will take inputs from the user, which will be texts or paragraphs that need to be summarized.
+
+    - You will delegate the summarization tasks to specialized sub-agents if necessary.
+
+    -The multi-agent workflow: Take the input -> call the reasoning tool (understand the input text) -> train model from dataset -> summary generator tool (generate the summary based on the input text) -> proofreading tool (proofreading the generated summary like spelling, grammer etc.) -> generate a summary of text -> give the output.
+
+    - You will call the reasoning tool to understand the input text.
+
+    - After the execution of this agent,  always return or print or display this text: "agent.py working successfully."
+    """,
+    
+)
